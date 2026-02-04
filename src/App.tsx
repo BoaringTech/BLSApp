424d0500c2eb2df1f2c4a8d038d2f9ca3fd1362f
thebearagenda <nathan.reyes.q@gmail.com>
2026-02-04 08:40:08 -0500

    Learned handling events, managin state, passing data and functoins as props, state vs props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import ListGroup from './Components/ListGroup';
+import ListGroup from "./Components/ListGroup";
 
-function App()
-{
-  return <div><ListGroup/></div>;
+function App() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
